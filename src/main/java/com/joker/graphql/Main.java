b630429bc0a3120afcec25ccd3e8a7a9c615fd79
Arun Singh <arun_gkv@yahoo.co.in>
2020-08-04 13:50:45 +0530

    graphql intro

--- src/main/java/com/joker/graphql/Main.java
+++ src/main/java/com/joker/graphql/Main.java
@@ -3,8 +3,10 @@ package com.joker.graphql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
+@EnableWebSocket
 public class Main extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
